e5a8985cca1ecdea40226d0071d4023dcc9da671
santiarias65 <santiariasg1@gmail.com>
2025-04-08 19:00:17 -0500

    [ADDED] backend prod

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://env-qraccess-server.eba-83unvpkp.us-east-2.elasticbeanstalk.com/';
 const EMPTY_STRING = '';
 
 export {
