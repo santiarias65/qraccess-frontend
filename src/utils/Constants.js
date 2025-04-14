65ef85eb237ec9b352b5a0703bb9f94b6c26c5ba
santiarias65 <santiariasg1@gmail.com>
2025-04-13 21:20:31 -0500

    [UPD] new url entorno

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://env-qraccess-server.eba-83unvpkp.us-east-2.elasticbeanstalk.com/';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://qraccess-server-env.eba-83unvpkp.us-east-2.elasticbeanstalk.com/';
 const EMPTY_STRING = '';
 
 export {
